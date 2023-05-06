8a958de9fdaebd5045d6faa3c2c5c8c531d3babf
Dillon Ellis <dillonzellis@gmail.com>
2023-05-06 11:13:40 -0400

    placeholder socials

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ const Home = () => {
     <div className="container mx-auto text-white">
       <div className="flex min-h-screen flex-col items-center justify-center gap-4">
         <div className="text-5xl">Zellis</div>
-        <div>Coming Soon</div>
-        <div className="flex gap-8">
+        <div className="mb-8">Coming Soon</div>
+        <div className="flex flex-col flex-wrap gap-2 text-center md:flex-row md:gap-8">
           <div>youtube</div>
-          <div>youtube</div>
-          <div>youtube</div>
+          <div>instagram</div>
+          <div>twitter</div>
+          <div>tiktok</div>
+          <div>Spotify</div>
         </div>
       </div>
     </div>
