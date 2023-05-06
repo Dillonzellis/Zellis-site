15ae9838bb1835cb12c3b83bf08c6bef91e97951
Dillon Ellis <dillonzellis@gmail.com>
2023-05-06 11:16:17 -0400

    more typography styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <div className="container mx-auto text-white">
       <div className="flex min-h-screen flex-col items-center justify-center gap-4">
-        <div className="text-5xl">Zellis</div>
-        <div className="mb-8">Coming Soon</div>
-        <div className="flex flex-col flex-wrap gap-2 text-center md:flex-row md:gap-8">
+        <div className="text-5xl font-bold">Zellis</div>
+        <div className="mb-8 text-xl">Coming Soon</div>
+        <div className="flex flex-col flex-wrap gap-2 text-center capitalize md:flex-row md:gap-8">
           <div>youtube</div>
+          <div>Spotify</div>
           <div>instagram</div>
           <div>twitter</div>
           <div>tiktok</div>
-          <div>Spotify</div>
         </div>
       </div>
     </div>
